5dd618d5a0b7035ba871334ee506b21765b59962
adekang <201497709@qq.com>
2021-05-07 20:58:29 +0800

    deploy

--- __tests__/db.spec.js
+++ __tests__/db.spec.js
@@ -1,9 +1,26 @@
 const db = require('../db.js')
+const fs = require('fs')
+jest.mock('fs')
+
 describe('db', () => {
-  it('can read', function () {
-    expect(db.read instanceof Function).toBe(true)
+  afterEach(() => {
+    fs.clearMocks()
   })
-  it('can write', function () {
-    expect(db.write instanceof Function).toBe(true)
+  it('can read', async () => {
+    const data = [{title: 'hi', done: true}]
+    fs.setReadFileMock('/xxx', null, JSON.stringify(data))
+    const list = await db.read('/xxx')
+    expect(list).toStrictEqual(data)
+  })
+  it('can write', async () => {
+    let fakeFile
+    fs.setWriteFileMock('/yyy', (path, data, callback) => {
+      fakeFile = data
+      callback(null)
+    })
+    const list = [{title: '见欧阳娜娜', done: true}, {title: '见迪丽热巴', done: true}]
+    await db.write(list, '/yyy')
+    expect(fakeFile).toBe(JSON.stringify(list) + '\n')
   })
 })
+
